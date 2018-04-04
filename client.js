a49acd1519adc73ec76db61a23452c711c9fca0f
Steve Sefchick <stephen.sefchick@wawa.com>
2018-04-03 23:20:23 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -2,7 +2,15 @@ TrelloPowerUp.initialize({
   'card-buttons': function(t, options){
     return [{
       icon: 'https://rawgit.com/stevesefchick/trelloreleasenotes/master/hawkicon.jpg.jpg',
-      text: 'Release Note Time!',
+      text: 'Release Note!',
+	  callback: function(t){
+		  return t.popup({
+		  title: "Friendly Release Notes",
+		  url: 'https://rawgit.com/stevesefchick/trelloreleasenotes/master/releasenoteselection.html'
+		  
+	  });
+		  
+	  }
     }];
   },
 });
