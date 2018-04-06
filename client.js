201d161f28bc0d7b964b9967dfe7eb0cb176b0bf
Steve Sefchick <stephen.sefchick@wawa.com>
2018-04-05 23:33:45 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ TrelloPowerUp.initialize({
 	  callback: function(t){
 		  return t.popup({
 		  title: "Release Notes",
-		  url: 'https://rawgit.com/stevesefchick/trelloreleasenotes/master/releasenoteselection.html'
+		  url: 'https://rawgit.com/stevesefchick/trelloreleasenotes/master/releasenotes.html'
 		  
 	  });
 		  
