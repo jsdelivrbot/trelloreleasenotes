bc52a1e26e2b10e764915b80377723a39fa15f90
Steve Sefchick <stephen.sefchick@wawa.com>
2018-04-08 15:26:02 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -16,9 +16,9 @@ TrelloPowerUp.initialize({
     'card-badges': function(t, options) {
 		return t.get('card','shared','releasenotes')
 		.then(function(releasenotes) {
+			console.log(releasenotes);
 			if (releasenotes !== undefined)
 			{
-				console.log(releasenotes);
 					return [{
 					 // icon: releasenotes ? null : 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717'
 					  icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717'
